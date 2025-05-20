01e65027bf9169b591a5d27c9a3e09ca4d24faa0
MrAce801 <sandro.rappel@gmail.com>
2025-05-20 18:56:35 +0200

    Update App.js: Mobile-Optimierung

--- src/App.js
+++ src/App.js
@@ -588,7 +588,7 @@ export default function App(){
                           <div style={{
                             position:"absolute",top:0,left:0,
                             width:"100%",height:"100%",
-                            backdropFilter:"blur(8px)",
+                            backdropFilter:"blur(6px)",
                             backgroundColor:"rgba(0,0,0,0.2)",
                             borderRadius:8
                           }}/>
