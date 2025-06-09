dc97f52539dda2ad50da13e021d4312b827825c3
MrAce801 <sandro.rappel@gmail.com>
2025-06-09 12:15:42 +0200

    Adjust new entry quick arrow offset

--- src/components/NewEntryForm.js
+++ src/components/NewEntryForm.js
@@ -49,8 +49,8 @@ export default function NewEntryForm({
             ...styles.glassyIconButton(dark),
             padding: '4px',
             position: 'absolute',
-            top: 'calc(50% - 10px)',
-            right: '46px',
+            top: 'calc(50% - 5px)',
+            right: '48px',
             transform: 'translateY(-50%)',
             color: '#333'
           }}
