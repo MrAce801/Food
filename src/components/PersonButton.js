a21833d711451a5a1de919e197a95b5554e2b468
MrAce801 <sandro.rappel@gmail.com>
2025-06-12 15:58:23 +0200

    Remove insights feature and add personal info modal

---
+++ src/components/PersonButton.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "../styles";
+
+const PersonButton = ({ onClick, dark }) => (
+  <button
+    onClick={onClick}
+    className="haptic"
+    title="Personendaten"
+    style={styles.glassyIconButton(dark)}
+  >
+    🧍
+  </button>
+);
+
+export default PersonButton;
